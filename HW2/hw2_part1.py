91fc6b60ba6b33d54ae44b634fe3f8b8974d7670
xz4535 <144400181+xz4535@users.noreply.github.com>
2024-02-23 01:26:30 -0500

    add all quesrion in part 1

--- HW2/hw2_part1.py
+++ HW2/hw2_part1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
 # USAGE:
@@ -27,56 +27,56 @@ with sqlite3.connect(db_file) as conn:
 
     # ADD YOUR CODE STARTING HERE
     
-    # Question 1
+    # Question 1:Which tracks (ids and names) have a lyricist whose name begins with "W"?
     print('Question 1:')
     
     # implement your solution to q1
     
     print('---')
     
-    # Question 2
+    # Question 2:What are the values that can be taken by the track.track_explicit field?
     print('Question 2:')
     
     # implement your solution to q2
     
     print('---')
     
-    # Question 3
+    # Question 3:Which track (id and title) has the most listens?
     print('Question 3:')
     
     # implement your solution to q3
     
     print('---')
     
-    # Question 4
+    # Question 4:How many artists have "related projects"?
     print('Question 4:')
     
     # implement your solution to q4
     
     print('---')
     
-    # Question 5
+    # Question 5:Which non-null language codes have exactly 4 tracks?
     print('Question 5:')
     
     # implement your solution to q5
     
     print('---')
     
-    # Question 6
+    # Question 6:How many tracks are by artists known to be active only within the 1990s?
     print('Question 6:')
     
     # implement your solution to q6
     
     print('---')
     
-    # Question 7
+    # Question 7:Which three artists have worked with the largest number of distinct album producers?
     print('Question 7:')
     
     # implement your solution to q7
     
     print('---')
     
-    # Question 8
+    # Question 8:Which track (include id, title, and artist name) has the largest difference between the number of album listens and track listens?
     print('Question 8:')
     
     # implement your solution to q8
